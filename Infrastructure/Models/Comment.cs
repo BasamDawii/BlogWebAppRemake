fbc83372c25d9338f6e828ae1ec78c6cdc99be83
BasamDawii <basa0033@easv365.dk>
2024-01-11 22:27:32 +0100

    project initial structure  has been created

---
+++ Infrastructure/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int BlogId { get; set; }
+    public string Text { get; set; }
+    public DateTime PublicationDate { get; set; }
+    public string UserFullName { get; set; }
+}
