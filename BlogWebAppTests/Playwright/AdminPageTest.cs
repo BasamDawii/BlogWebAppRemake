960b1690979e5a8465dd07f6625e08f9a8fe8def
BasamDawii <basa0033@easv365.dk>
2024-01-13 20:20:57 +0100

    playwright tests have been implemented and workflow has been updated

---
+++ BlogWebAppTests/Playwright/AdminPageTest.cs
@@ -0,0 +1,43 @@
+﻿namespace BlogWebAppTests.Playwright;
+
+[TestFixture]
+public class AdminPageTest : AuthenticatedPageTest
+{
+    [SetUp]
+    public new async Task SetUp()
+    {
+        await base.SetUp();
+        await Page.GotoAsync("http://localhost:4200/admin/admin-home");
+    }
+    
+    [TearDown]
+    public void Teardown()
+    {
+        Helper.TriggerRebuild();
+    }
+    
+    [Test]
+    public async Task ShouldListBlogsAndDeleteFirstBlog()
+    {
+        var blogPosts = Page.Locator("ion-card");
+        await Expect(blogPosts).ToHaveCountAsync(3, new() { Timeout = 5000 });
+        
+        var firstBlogDeleteButton = Page.Locator("ion-card:first-of-type ion-button[color='danger']");
+        await Expect(firstBlogDeleteButton).ToBeVisibleAsync();
+        
+        await firstBlogDeleteButton.ClickAsync();
+        var confirmDialog = Page.Locator(".alert-wrapper");
+        await Expect(confirmDialog).ToBeVisibleAsync(new() { Timeout = 5000 });
+        
+        var confirmButton = confirmDialog.Locator("text='Yes'");
+        await confirmButton.ClickAsync();
+        
+        await Expect(confirmDialog).ToBeHiddenAsync();
+        
+        var addButton = Page.Locator("ion-fab-button");
+        await addButton.ClickAsync();
+        
+        await Expect(Page).ToHaveURLAsync("http://localhost:4200/admin/create-blog");
+        
+    }
+}
